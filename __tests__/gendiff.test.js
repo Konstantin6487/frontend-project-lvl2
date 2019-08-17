9f28638d83720fe16e97c56bbfd0e9a38a628349
Konstantin6487 <Konstantin6487@rambler.ru>
2019-08-17 19:27:30 +0300

    step3 fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,25 +2,30 @@ import fs from 'fs';
 import { resolve, join } from 'path';
 import gendiff from '../src';
 
-const fixturesFolderPath = resolve(__dirname, '__fixtures__');
-const getFilePath = (fileName) => join(fixturesFolderPath, fileName);
+const buildDirPath = (dirName) => resolve(__dirname, dirName);
+const buildPathFromDir = (dirPath) => (fileName) => join(dirPath, fileName);
 
-test('test1: .json files', () => {
-  const fileBeforePath = getFilePath('before.json');
-  const fileAfterPath = getFilePath('after.json');
-  const resultPath = getFilePath('result');
+describe('Gendiff tests', () => {
+  const fixturesDirPath = buildDirPath('__fixtures__');
+  const buildPath = buildPathFromDir(fixturesDirPath);
 
-  const expected = fs.readFileSync(resultPath, 'utf8');
-  const result = gendiff(fileBeforePath, fileAfterPath);
-  expect(result).toBe(expected);
-});
+  test('test1: .json files', () => {
+    const fileBeforePath = buildPath('before.json');
+    const fileAfterPath = buildPath('after.json');
+    const resultPath = buildPath('result');
 
-test('test2: .yml files', () => {
-  const fileBeforePath = getFilePath('before.yml');
-  const fileAfterPath = getFilePath('after.yml');
-  const resultPath = getFilePath('result');
+    const expected = fs.readFileSync(resultPath, 'utf8');
+    const result = gendiff(fileBeforePath, fileAfterPath);
+    expect(result).toBe(expected);
+  });
 
-  const expected = fs.readFileSync(resultPath, 'utf8');
-  const result = gendiff(fileBeforePath, fileAfterPath);
-  expect(result).toBe(expected);
+  test('test2: .yml files', () => {
+    const fileBeforePath = buildPath('before.yml');
+    const fileAfterPath = buildPath('after.yml');
+    const resultPath = buildPath('result');
+
+    const expected = fs.readFileSync(resultPath, 'utf8');
+    const result = gendiff(fileBeforePath, fileAfterPath);
+    expect(result).toBe(expected);
+  });
 });
