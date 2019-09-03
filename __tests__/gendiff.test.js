b6c689f95682cfed0611233b8bc5b0a69deab26b
Konstantin6487 <Konstantin6487@rambler.ru>
2019-09-04 00:33:01 +0300

    test refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,41 +1,29 @@
 import fs from 'fs';
-import { resolve, join } from 'path';
+import { resolve, join, extname } from 'path';
 import gendiff from '../src';
 
+const testCasesFiles = [
+  ['before.json', 'after.json', 'result.diffjson'],
+  ['before.yml', 'after.yml', 'result.plain'],
+  ['before.ini', 'after.ini', 'result.json'],
+];
+
+const getFormatType = (filePath) => extname(filePath).slice(1);
 const buildAbsDirPath = (dirName) => resolve(__dirname, dirName);
 const buildFilePathInDir = (dirPath) => (fileName) => join(dirPath, fileName);
 
 describe('Gendiff tests', () => {
   const fixturesDirPath = buildAbsDirPath('__fixtures__');
   const buildFilePath = buildFilePathInDir(fixturesDirPath);
-  const buildFilePathsArr = (fileNames) => fileNames.map(buildFilePath);
+  const buildFilePaths = (fileNames) => fileNames.map(buildFilePath);
 
-  test.each([['before.json', 'after.json', 'result.diffjson']]
-    .map(buildFilePathsArr))(
-    'Format diff output to "diffjson" format',
-    (fileBeforePath, fileAfterPath, fileDiffPath) => {
-      const result = gendiff(fileBeforePath, fileAfterPath);
-      const expected = fs.readFileSync(fileDiffPath, 'utf8');
-      expect(result).toBe(expected);
-    },
-  );
+  test.each(testCasesFiles.map(buildFilePaths))(
+    "generate correct file's diff N%#",
+    (beforeFilePath, afterFilePath, resultFilePath) => {
+      const diffFormat = getFormatType(resultFilePath);
+      const result = gendiff(beforeFilePath, afterFilePath, diffFormat);
+      const expected = fs.readFileSync(resultFilePath, 'utf8');
 
-  test.each([['before.yml', 'after.yml', 'result.plain']]
-    .map(buildFilePathsArr))(
-    'Format diff output to "plain" format',
-    (fileBeforePath, fileAfterPath, fileDiffPath) => {
-      const result = gendiff(fileBeforePath, fileAfterPath, 'plain');
-      const expected = fs.readFileSync(fileDiffPath, 'utf8');
-      expect(result).toBe(expected);
-    },
-  );
-
-  test.each([['before.ini', 'after.ini', 'result.json']]
-    .map(buildFilePathsArr))(
-    'Format diff output to "json" format',
-    (fileBeforePath, fileAfterPath, fileDiffPath) => {
-      const result = gendiff(fileBeforePath, fileAfterPath, 'json');
-      const expected = fs.readFileSync(fileDiffPath, 'utf8');
       expect(result).toBe(expected);
     },
   );
