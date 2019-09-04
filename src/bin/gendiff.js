b6e61a0ebf0bc016f4a8eadd0510c31568162285
Konstantin6487 <Konstantin6487@rambler.ru>
2019-09-05 01:13:56 +0300

    refactoring bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,8 +7,8 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [diffjson | plain | json]', 'output diffjson, plain or json diff')
-  .arguments('<firstConfig> <secondConfig>')
-  .action((arg1, arg2) => {
-    console.log(genDiff(arg1, arg2, program.format));
+  .arguments('<originalConfig> <newConfig>')
+  .action((original, newest) => {
+    console.log(genDiff(original, newest, program.format));
   })
   .parse(process.argv);
