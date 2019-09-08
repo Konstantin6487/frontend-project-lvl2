fe044fc14639ca2f7b6116bef6fb6196e275953e
Konstantin6487 <Konstantin6487@rambler.ru>
2019-09-08 23:46:52 +0300

    refactoring genDiff

--- src/index.js
+++ src/index.js
@@ -3,72 +3,65 @@ import {
   identity,
   isPlainObject,
   union,
-  isEmpty,
 } from 'lodash';
 import { getFormat, getData } from './helpers';
 import parseData from './parsers';
 import getRenderFormat from './formatters';
 
-const FLAG_ADDED_VALUE = '+';
-const FLAG_REMOVED_VALUE = '-';
-const FLAG_SAME_VALUE = ' ';
+const makeDiffNode = (type) => (originalData, newData, key, fn) => {
+  const newValue = newData[key];
+  const originalValue = originalData[key];
 
-const makeLeafNode = (key, value, changeFlag = FLAG_SAME_VALUE) => ({
-  key,
-  value,
-  changeFlag,
-  type: 'leaf',
-});
-
-const makeInnerNode = (key, children, changeFlag = FLAG_SAME_VALUE) => ({
-  key,
-  children,
-  changeFlag,
-  type: 'inner',
-});
-
-const makeNode = (value, value2) => (key, changeFlag, fn) => {
-  if (value && value2) {
-    return makeInnerNode(key, fn(value, value2), changeFlag);
-  }
-  return isPlainObject(value)
-    ? makeInnerNode(key, fn(value), changeFlag)
-    : makeLeafNode(key, value, changeFlag);
+  const typesActions = {
+    added: () => ({ type, value: newValue, key }),
+    removed: () => ({ type, value: originalValue, key }),
+    nested: () => ({ type, children: fn(originalValue, newValue), key }),
+    changed: () => ({
+      type,
+      newValue,
+      originalValue,
+      key,
+    }),
+    unchanged: () => ({ type, value: originalValue, key }),
+  };
+  const makeTypedNode = typesActions[type];
+  return makeTypedNode();
 };
 
-const typeChangesActions = [
+const nodeTypesActions = [
   {
-    type: 'added',
-    cond: (original, newest, key) => !has(original, key) && has(newest, key),
-    action: (original, newest, key, fn) => makeNode(newest[key])(key, isEmpty(original)
-      ? FLAG_SAME_VALUE
-      : FLAG_ADDED_VALUE, fn),
+    check: (originalData, newData, key) => [
+      !has(originalData, key),
+      has(newData, key),
+    ].every(Boolean),
+    action: makeDiffNode('added'),
   },
   {
-    type: 'removed',
-    cond: (original, newest, key) => has(original, key) && !has(newest, key),
-    action: (original, newest, key, fn) => makeNode(original[key])(key, isEmpty(newest)
-      ? FLAG_SAME_VALUE
-      : FLAG_REMOVED_VALUE, fn),
+    check: (originalData, newData, key) => [
+      has(originalData, key),
+      !has(newData, key),
+    ].every(Boolean),
+    action: makeDiffNode('removed'),
   },
   {
-    type: 'nested',
-    cond: (original, newest, key) => has(original, key) && has(newest, key)
-      && [original[key], newest[key]].every(isPlainObject),
-    action: (original, newest, key, fn) => makeNode(original[key], newest[key])(key, FLAG_SAME_VALUE, fn),
+    check: (originalData, newData, key) => [
+      has(originalData, key),
+      has(newData, key),
+      [newData[key], originalData[key]].every(isPlainObject),
+    ].every(Boolean),
+    action: makeDiffNode('nested'),
   },
   {
-    type: 'unchanged',
-    cond: (original, newest, key) => original[key] === newest[key],
-    action: (_, newest, key) => makeNode(newest[key])(key),
+    check: (originalData, newData, key) => [
+      has(originalData, key),
+      has(newData, key),
+      originalData[key] !== newData[key],
+    ].every(Boolean),
+    action: makeDiffNode('changed'),
   },
   {
-    type: 'changed',
-    cond: () => identity,
-    action: (original, newest, key, fn) => [
-      makeNode(newest[key])(key, FLAG_ADDED_VALUE, fn),
-      makeNode(original[key])(key, FLAG_REMOVED_VALUE, fn),
-    ],
+    check: identity,
+    action: makeDiffNode('unchanged'),
   },
 ];
 
@@ -77,14 +70,14 @@ const makeAstDiff = (originalData = {}, newData = {}) => {
   const newDataKeys = Object.keys(newData);
   const unionDataKeys = union(originalDataKeys, newDataKeys);
   return unionDataKeys.map((key) => {
-    const currentTypeChangeData = typeChangesActions
-      .find(({ cond }) => cond(originalData, newData, key));
-    const { action } = currentTypeChangeData;
+    const checkedCurrentNodeType = nodeTypesActions
+      .find(({ check }) => check(originalData, newData, key));
+    const { action } = checkedCurrentNodeType;
     return action(originalData, newData, key, makeAstDiff);
   });
 };
 
-export default (originalFilePath, newFilePath, formatType = 'diffjson') => {
+export default (originalFilePath, newFilePath, formatType = 'json') => {
   const originalFileFormat = getFormat(originalFilePath);
   const originalFileData = getData(originalFilePath);
   const parsedOriginalFile = parseData(originalFileFormat, originalFileData);
@@ -94,6 +87,8 @@ export default (originalFilePath, newFilePath, formatType = 'diffjson') => {
   const parsedNewFile = parseData(newFileFormat, newFileData);
 
   const astDiff = makeAstDiff(parsedOriginalFile, parsedNewFile);
+  console.log(JSON.stringify(astDiff));
+  console.log(astDiff);
   const format = getRenderFormat(formatType);
   const renderedDiff = format(astDiff);
   return renderedDiff;
