4a330040451816f4087df3dc4b271873a594278a
Konstantin6487 <Konstantin6487@rambler.ru>
2019-09-15 16:26:30 +0300

    refactoring diffJsonFormat

--- src/formatters/diffJsonFormat.js
+++ src/formatters/diffJsonFormat.js
@@ -1,19 +1,23 @@
 import { isPlainObject } from 'lodash';
 
-const indentSize = 4;
+const defaultIndentSize = 4;
 
-const stringifyNodeValue = (nodeValue, prevIndent) => {
+const stringifyNodeIndent = (indentSize) => ' '.repeat(indentSize);
+
+const stringifyNodeValue = (nodeValue, prevIndentSize) => {
   if (isPlainObject(nodeValue)) {
     const startBracket = '{\n';
-    const endBracket = `${' '.repeat(prevIndent)}}`;
+    const stringifiedIndent = stringifyNodeIndent(prevIndentSize);
+    const endBracket = `${stringifiedIndent}}`;
 
     const stringifiedValue = Object
       .keys(nodeValue)
       .map((key) => {
-        const newIndent = ' '.repeat(prevIndent + indentSize);
+        const innerIndentSize = prevIndentSize + defaultIndentSize;
+        const stringifiedInnerIndent = stringifyNodeIndent(innerIndentSize);
         const value = nodeValue[key];
-        const stringified = stringifyNodeValue(value, newIndent);
-        return `${newIndent}${key}: ${stringified}\n`;
+        const stringified = stringifyNodeValue(value, innerIndentSize);
+        return `${stringifiedInnerIndent}${key}: ${stringified}\n`;
       })
       .join('');
 
@@ -24,20 +28,20 @@ const stringifyNodeValue = (nodeValue, prevIndent) => {
 
 const typesRenders = {
   added: ({ key, value }, depth) => {
-    const innerIndent = depth * indentSize;
-    return `${' '.repeat(innerIndent - 2)}+ ${key}: ${stringifyNodeValue(value, innerIndent)}`;
+    const prevIndentSize = depth * defaultIndentSize;
+    return `${stringifyNodeIndent(prevIndentSize - 2)}+ ${key}: ${stringifyNodeValue(value, prevIndentSize)}`;
   },
   removed: ({ key, value }, depth) => {
-    const innerIndent = depth * indentSize;
-    return `${' '.repeat(innerIndent - 2)}- ${key}: ${stringifyNodeValue(value, innerIndent)}`;
+    const prevIndentSize = depth * defaultIndentSize;
+    return `${stringifyNodeIndent(prevIndentSize - 2)}- ${key}: ${stringifyNodeValue(value, prevIndentSize)}`;
   },
   nested: ({ key, children }, depth, fn) => {
-    const innerIndent = depth * indentSize;
-    return `${' '.repeat(innerIndent)}${key}: ${fn(children, depth + 1)}`;
+    const prevIndentSize = depth * defaultIndentSize;
+    return `${stringifyNodeIndent(prevIndentSize)}${key}: ${fn(children, depth + 1)}`;
   },
   unchanged: ({ key, value }, depth) => {
-    const innerIndent = depth * indentSize;
-    return `${' '.repeat(innerIndent)}${key}: ${stringifyNodeValue(value, innerIndent)}`;
+    const prevIndentSize = depth * defaultIndentSize;
+    return `${stringifyNodeIndent(prevIndentSize)}${key}: ${stringifyNodeValue(value, prevIndentSize)}`;
   },
   changed: ({ key, newValue, originalValue }, depth) => (
     `${typesRenders.added({ key, value: newValue }, depth)}\n${typesRenders.removed({ key, value: originalValue }, depth)}`
@@ -52,7 +56,8 @@ const format = (ast, depth = 1) => {
     })
     .join('');
   const startBracket = '{\n';
-  const endBracket = `${' '.repeat(depth === 1 ? 0 : (depth - 1) * indentSize)}}`;
+  const innerIndentSize = depth === 1 ? 0 : (depth - 1) * defaultIndentSize;
+  const endBracket = `${stringifyNodeIndent(innerIndentSize)}}`;
 
   return `${startBracket}${renderedTypes}${endBracket}`;
 };
