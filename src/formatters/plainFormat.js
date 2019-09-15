4ef40cc5197fa4a243c1a6a4f1b9fade3eb4fe27
Konstantin6487 <Konstantin6487@rambler.ru>
2019-09-15 14:55:41 +0300

    refactoring plainFormat

--- src/formatters/plainFormat.js
+++ src/formatters/plainFormat.js
@@ -1,54 +1,27 @@
-import { get } from 'lodash';
+import { isPlainObject, identity, stubString } from 'lodash';
 
-const isChanged = Array.isArray;
-const isRemoved = ({ changeFlag }) => changeFlag === '-';
-const isAdded = ({ changeFlag }) => changeFlag === '+';
-const isUnchanged = ({ changeFlag }) => changeFlag === ' ';
+const stringifyNodePath = (path) => `'${path.join('.')}'`;
+const stringifyNodeValue = (nodeValue) => (isPlainObject(nodeValue) ? '[complex value]' : nodeValue);
+const stringifyChangesList = (list) => list.map((node) => `Property ${node}`).join('\n');
 
-const formatNodeValue = (n) => (n.type === 'inner' ? '[complex value]' : n.value);
-const formatNodePath = (keys) => keys.join('.');
-
-const typeChangesMessages = [
-  {
-    type: 'changed',
-    cond: isChanged,
-    getMessage: ([prevValue, nextValue], keys) => `Property '${formatNodePath(keys)}' was updated. From ${formatNodeValue(nextValue)} to ${formatNodeValue(prevValue)}`,
-  },
-  {
-    type: 'removed',
-    cond: isRemoved,
-    getMessage: (_, keys) => `Property '${formatNodePath(keys)}' was removed`,
-  },
-  {
-    type: 'added',
-    cond: isAdded,
-    getMessage: (n, keys) => `Property '${formatNodePath(keys)}' was added with value: ${formatNodeValue(n)}`,
-  },
-  {
-    type: 'unchanged',
-    cond: isUnchanged,
-    getMessage: () => '',
-  },
-];
-
-const format = (ast, path = []) => {
-  const getNodeKey = (n) => (isChanged(n) ? get(n, '0.key') : get(n, 'key'));
-
-  const stringifyNodeValue = (node, fn, pathKeys) => {
-    const currentNodeMessage = typeChangesMessages.find(({ cond }) => cond(node));
-    return node.type === 'inner' && isUnchanged(node)
-      ? fn(node.children, pathKeys)
-      : currentNodeMessage.getMessage(node, pathKeys);
-  };
-
-  return ast
-    .map((node) => stringifyNodeValue(
-      node,
-      format,
-      path.concat(getNodeKey(node)),
-    ))
-    .filter(Boolean)
-    .join('\n');
+const typesRenders = {
+  added: ({ value }, path) => `${stringifyNodePath(path)} was added with value: ${stringifyNodeValue(value)}`,
+  removed: (_, path) => `${stringifyNodePath(path)} was removed`,
+  nested: ({ children }, depth, fn) => fn(children, depth),
+  unchanged: stubString,
+  changed: ({ newValue, originalValue }, path) => `${stringifyNodePath(path)} was updated. From ${stringifyNodeValue(originalValue)} to ${stringifyNodeValue(newValue)}`,
 };
 
-export default format;
+export default (ast) => {
+  const getChangesList = (tree, path = []) => tree
+    .map((node) => {
+      const { key, type } = node;
+      const renderType = typesRenders[type];
+      return renderType(node, path.concat(key), getChangesList);
+    })
+    .filter(identity)
+    .flat();
+  const changesList = getChangesList(ast);
+  const stringifiedChanges = stringifyChangesList(changesList);
+  return stringifiedChanges;
+};
