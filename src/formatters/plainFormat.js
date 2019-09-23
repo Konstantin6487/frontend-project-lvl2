5ab1b92080e6544b5a45bd4bcb920b0e019a1532
Konstantin Bochinin <Konstantin6487@nodasoft.com>
2019-09-23 17:56:55 +0300

    refactoring plainFormat, remove pipeline

--- src/formatters/plainFormat.js
+++ src/formatters/plainFormat.js
@@ -1,15 +1,14 @@
 import { isEmpty, isPlainObject, identity } from 'lodash';
 
-const stringifyNodePath = (path) => `'${path.join('.')}'`;
+const stringifyNodePath = (path) => path.join('.');
 const stringifyNodeValue = (nodeValue) => (isPlainObject(nodeValue) ? '[complex value]' : nodeValue);
-const stringifyChangesList = (list) => list.map((node) => `Property ${node}`).join('\n');
 
 const typesRenders = {
-  added: ({ value }, path) => `${stringifyNodePath(path)} was added with value: ${stringifyNodeValue(value)}`,
-  removed: (_, path) => `${stringifyNodePath(path)} was removed`,
+  added: ({ value }, path) => `Property '${stringifyNodePath(path)}' was added with value: ${stringifyNodeValue(value)}`,
+  removed: (_, path) => `Property '${stringifyNodePath(path)}' was removed`,
   nested: ({ children }, path, fn) => fn(children, path),
   unchanged: () => null,
-  changed: ({ newValue, originalValue }, path) => `${stringifyNodePath(path)} was updated. From ${stringifyNodeValue(originalValue)} to ${stringifyNodeValue(newValue)}`,
+  changed: ({ newValue, originalValue }, path) => `Property '${stringifyNodePath(path)}' was updated. From ${stringifyNodeValue(originalValue)} to ${stringifyNodeValue(newValue)}`,
 };
 
 export default (ast) => {
@@ -24,5 +23,5 @@ export default (ast) => {
     })
     .filter(identity)
     .flat();
-  return ast |> getChangesList |> stringifyChangesList;
+  return getChangesList(ast).join('\n');
 };
