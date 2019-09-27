a5567bcd4ef45eb8d9460d708a3b09c5ad9a7e11
Konstantin6487 <Konstantin6487@rambler.ru>
2019-09-28 02:53:56 +0300

    refactoring pretty, plain formatters

--- src/formatters/plainFormat.js
+++ src/formatters/plainFormat.js
@@ -4,11 +4,15 @@ const stringifyNodePath = (path) => path.join('.');
 const stringifyNodeValue = (nodeValue) => (isPlainObject(nodeValue) ? '[complex value]' : nodeValue);
 
 const typesRenders = {
-  added: ({ value }, path) => `Property '${stringifyNodePath(path)}' was added with value: ${stringifyNodeValue(value)}`,
+  added: ({ value }, path) => (
+    `Property '${stringifyNodePath(path)}' was added with value: ${stringifyNodeValue(value)}`
+  ),
   removed: (_, path) => `Property '${stringifyNodePath(path)}' was removed`,
   nested: ({ children }, path, fn) => fn(children, path),
   unchanged: () => null,
-  changed: ({ newValue, originalValue }, path) => `Property '${stringifyNodePath(path)}' was updated. From ${stringifyNodeValue(originalValue)} to ${stringifyNodeValue(newValue)}`,
+  changed: ({ newValue, originalValue }, path) => (
+    `Property '${stringifyNodePath(path)}' was updated. From ${stringifyNodeValue(originalValue)} to ${stringifyNodeValue(newValue)}`
+  ),
 };
 
 export default (ast) => {
--- src/formatters/prettyFormat.js
+++ src/formatters/prettyFormat.js
@@ -8,7 +8,6 @@ const stringifyValue = (nodeValue, depth) => {
   if (!isPlainObject(nodeValue)) {
     return nodeValue;
   }
-
   const indent = fillIndent(depth);
   const stringified = Object
     .keys(nodeValue)
@@ -23,9 +22,15 @@ const stringifyValue = (nodeValue, depth) => {
 };
 
 const nodeTypesActions = {
-  added: ({ key, value }, depth) => `${fillIndent(depth)}${baseIndent.slice(0, -2)}+ ${key}: ${stringifyValue(value, depth + 1)}`,
-  removed: ({ key, value }, depth) => `${fillIndent(depth)}${baseIndent.slice(0, -2)}- ${key}: ${stringifyValue(value, depth + 1)}`,
-  unchanged: ({ key, value }, depth) => `${fillIndent(depth)}${baseIndent}${key}: ${stringifyValue(value, depth + 1)}`,
+  added: ({ key, value }, depth) => (
+    `${fillIndent(depth)}${baseIndent.slice(0, -2)}+ ${key}: ${stringifyValue(value, depth + 1)}`
+  ),
+  removed: ({ key, value }, depth) => (
+    `${fillIndent(depth)}${baseIndent.slice(0, -2)}- ${key}: ${stringifyValue(value, depth + 1)}`
+  ),
+  unchanged: ({ key, value }, depth) => (
+    `${fillIndent(depth)}${baseIndent}${key}: ${stringifyValue(value, depth + 1)}`
+  ),
   nested: ({ key, children }, depth, fn) => (
     `${fillIndent(depth)}${baseIndent}${key}: {\n${fn(children, depth + 1)}\n${fillIndent(depth)}${baseIndent}}`
   ),
@@ -39,7 +44,6 @@ export default (ast) => {
   if (isEmpty(ast)) {
     return '';
   }
-
   const inner = (tree, depth = 0) => tree
     .map((node) => {
       const action = nodeTypesActions[node.type];
@@ -48,6 +52,5 @@ export default (ast) => {
     .flat()
     .join('\n');
   const strinfified = inner(ast);
-
   return `{\n${strinfified}\n}`;
 };
